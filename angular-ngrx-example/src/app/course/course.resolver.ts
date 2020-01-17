02b1e2e22c86d846b9058bdcf90a4c4a4b657922
sarindufit <sarindufit@gmail.com>
2020-01-17 19:56:16 +0530

    src/app/course/course.resolver.ts

--- angular-ngrx-example/src/app/course/course.resolver.ts
+++ angular-ngrx-example/src/app/course/course.resolver.ts
@@ -18,7 +18,6 @@ export class CourseResolver implements Resolve<Observable<any>> {
     .pipe(
         select(areCoursesLoaded),
         tap((coursesLoaded) => {
-          console.log('111', coursesLoaded);
           if (!coursesLoaded) {
             this.store.dispatch(loadCourses());
           }
